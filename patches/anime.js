f630449f766b4216adfcda0318292495a2a28189
Jon Q <hello@jonquach.com>
2019-02-08 16:18:12 -0500

    Patches animejs for TypeScript

---
+++ patches/anime.js
@@ -0,0 +1,4 @@
+'use strict'
+Object.defineProperty(exports, '__esModule', {value: true})
+var anime = require('animejs').default
+exports.default = anime
--- src/anime.ts
+++ src/anime.ts
@@ -1,6 +1,6 @@
 // This file exists to ensure TypeScript correctly compiles import -> require
 // We need the compiled require to work like:
 // const anime = require('animejs').default
-import anime from 'animejs'
+import * as anime from 'animejs'
 
 export default anime
